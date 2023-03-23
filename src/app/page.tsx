c788952572c062a88745929347fdf688ef0c590f
SHAKRA-AMBREEN <shakraambreen150298@gmail.com>
2023-03-23 08:22:09 -0700

    remove underline updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Home from "@/components/Home";
 export default function Page() {
   return (
     <>
-    <h1 className="text-red-4xl font-bold underline">
+    
       <Home />
-    </h1>
+    
     </>
   )
 }
